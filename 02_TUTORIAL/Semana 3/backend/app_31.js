10fa125f576236627767133d887f40edc0970fcf
lyorrei-inteli <Lyorrei.Quintao@sou.inteli.edu.br>
2022-05-02 11:39:58 -0300

    Adicionei entregável da semana 3

---
+++ 02_TUTORIAL/Semana 3/backend/app_31.js
@@ -0,0 +1,17 @@
+
+const express = require('express'); 
+const app = express();
+
+const path = require('path')
+
+const hostname = '127.0.0.1';
+const port = 3031;
+app.use(express.static(path.join(__dirname + "/../frontend")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../index.html"))
+})
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
