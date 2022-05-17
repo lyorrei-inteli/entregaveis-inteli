3ccd21321967e4ba1e400e597ae4443280586b5b
lyorrei-inteli <Lyorrei.Quintao@sou.inteli.edu.br>
2022-05-17 11:38:00 -0300

    Autoestudo "Exercício de personalização - Etapa 4"

--- 03_AUT_EST_ENTREGA/Semana 2/public/script.js
+++ 03_AUT_EST_ENTREGA/Semana 2/public/script.js
@@ -1,10 +1,9 @@
-const newDiv = document.createElement("div")
-const paragraph = document.createElement("p")
-paragraph.innerText = "Linha inserida por meio do Javascript"
-paragraph.className = "jsParagraph"
-newDiv.append(paragraph)
+$(document).ready(() => {
+    const newDiv = document.createElement('div')
+    const paragraph = document.createElement('p')
+    paragraph.innerText = 'Linha inserida por meio do Javascript'
+    paragraph.className = 'jsParagraph'
+    newDiv.append(paragraph)
 
-
-
-let element = document.querySelector(".rightContainer")
-element.append(newDiv)
+    $('.rightContainer').append(newDiv)
+})
